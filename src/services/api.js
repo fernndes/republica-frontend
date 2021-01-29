d28ad59c456644425e52ca649f4da510f4752ed4
fernndes <you@example.com>
2021-01-29 20:34:30 -0300

    Axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import { baseURL } from './apiConfig'
-// import axios from 'axios'
+import axios from 'axios'
 
 // axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
 // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
